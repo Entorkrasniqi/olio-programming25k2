fa6adb210e226096ab779cf7f6188cc2396c148a
Entorkrasniqi <124073124+Entorkrasniqi@users.noreply.github.com>
2025-03-18 22:42:38 +0200

    task completed fam

---
+++ 1.1_module/src/task4.java
@@ -0,0 +1,25 @@
+public class task4 {
+    private String name;
+
+    public task4(String name) {
+        this.name = name;
+    }
+
+    public void meow() {
+        System.out.println("The cat named " + name + " says: Meow!");
+    }
+
+    public static void main(String[] args) {
+        // Luodaan kissat
+        task4 cat1 = new task4("Whiskers");
+        task4 cat2 = new task4("Rex");
+
+
+
+        // Tulostetaan vaadittu järjestys
+        cat1.meow();
+        cat2.meow();
+        cat1.meow();
+        cat1.meow();
+    }
+}
