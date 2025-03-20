f2629be17445614a90aaa8968e6e9d4bd94f81af
Entorkrasniqi <124073124+Entorkrasniqi@users.noreply.github.com>
2025-03-20 11:56:34 +0200

    upfa

---
+++ 1.3_module/src/binaryvalues.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class binaryvalues {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.err.println("Enter number: ");
+    }
+}
