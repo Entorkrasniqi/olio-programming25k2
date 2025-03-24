368f4ae5eebb67936e2af231e7196363856865cb
Entorkrasniqi <124073124+Entorkrasniqi@users.noreply.github.com>
2025-03-24 17:57:06 +0200

    dd

---
+++ 1.4_module/src/aNameGenerator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class aNameGenerator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter number bro");
+        
+        double a = Integer.parseInt(scanner.nextLine());
+    }
+}
