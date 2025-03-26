28fe94ea2feefe3305da93797a80b2cb4dfe7e03
Entorkrasniqi <124073124+Entorkrasniqi@users.noreply.github.com>
2025-03-26 11:48:22 +0200

    updt

--- 1.1_module/src/task2.java
+++ 1.1_module/src/task2.java
@@ -1,6 +1,6 @@
 public class task2 {
     public static void main(String[] args) {
-        int rows = 4; // Number of rows in the pyramid
+        int rows = 4; 
 
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= rows - i; j++) {
@@ -9,7 +9,7 @@ public class task2 {
             for (int k = 1; k <= (2 * i - 1); k++) {
                 System.out.print("*");
             }
-            System.out.println(); // Move to next line
+            System.out.println(); 
         }
     }
 }
---
+++ 2.1_module/src/televisionTask.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class televisionTask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter number: ");
+        double a = Double.parseDouble(scanner.nextLine());
+    }
+}
