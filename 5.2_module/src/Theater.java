176842d8f08103dfc259b121972737664654d56f
Entorkrasniqi <124073124+Entorkrasniqi@users.noreply.github.com>
2025-04-18 22:32:30 +0300

    tips

---
+++ 5.2_module/src/Theater.java
@@ -0,0 +1,20 @@
+public class Theater {
+    // step1: Create private seats as variable
+}
+
+//step 2: create a constructor
+
+// create methods to get and set the seats
+
+//method that tries to reserve a seat
+
+//method that tries to cancel a seat
+
+// method that checks if a seat is available
+
+// if yes then it reserves the seat
+
+// if not then it returns false
+
+
+// step 3:
