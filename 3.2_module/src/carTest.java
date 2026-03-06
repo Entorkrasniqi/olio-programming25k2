a1236858c696f5e492cae1617b3758e40cc40d3e
Entorkrasniqi <124073124+Entorkrasniqi@users.noreply.github.com>
2026-03-06 14:27:54 +0200

    update fields

--- 3.2_module/src/carTest.java
+++ 3.2_module/src/carTest.java
@@ -1,10 +1,12 @@
 public class carTest {
+    // a class which is called Car and inside it class's Data (fields) speed, name, weight, price
     class Car {
         private int speed;
         private String name;
         private double weight;
         private double price;
 
+        // constructor of Car class
         public Car(String name, int speed, double weight, double price) {
             this.name = name;
             this.speed = speed;
@@ -12,6 +14,7 @@ public class carTest {
             this.price = price;
         }
 
+        // getter method's for the name, speed, weight, price fields
         public int getSpeed() {
             return speed;
         }
@@ -28,4 +31,15 @@ public class carTest {
             return price;
         }
     }
+
+    public void accelerate(Car car) {
+        car.setSpeed(car.getSpeed() + 1);
+        System.out.println(car.getSpeed());
+    }
+
+    public void breakCar(Car car) {
+        car.setSpeed(car.getSpeed() - 1);
+        System.out.println(car.getSpeed());
+    }
 }
+
