b87f1894dc0e56aa35a71f4cea16f0a370dcb98d
Entorkrasniqi <124073124+Entorkrasniqi@users.noreply.github.com>
2026-08-25 14:14:14 +0300

    2.1 task 3 done

--- 2.1_module/src/task_three/coffeeMaker.java
+++ 2.1_module/src/task_three/coffeeMaker.java
@@ -1,17 +1,41 @@
 package task_three;
 
+// zone - 1 class and its variables(data)
 public class coffeeMaker {
     private boolean isOn;
     private String coffeeType;
     private int coffeeAmount; 
-    
+//zone - 2 constructors   
     public coffeeMaker() {
         this.isOn = false;
         this.coffeeType = "espresso";
         this.coffeeAmount = 10;
     }
+// zone - 3 methods (logic)
     public void switchOn() {
         isOn = true;
     }
+    public void switchOff () {
+        isOn = false;
+    }
+    public void setAmount(int coffeeAmount) {
+        if (isOn && coffeeAmount >= 10 && coffeeAmount <= 80) {
+            this.coffeeAmount = coffeeAmount;
+        }
+    }
+     public void setType(String coffeeType) {
+        if (isOn && (coffeeType.equals("espresso") || coffeeType.equals("normal"))) {
+            this.coffeeType = coffeeType;
+        }
+    }
+// zone - 4 accessor methods
+    public String getcoffeeType() {
+        return coffeeType;
+    }
+
+    public int getcoffeeAmount() {
+        return coffeeAmount;
+    }
+    
 }
 
--- 2.1_module/src/task_three/coffeeMakerDriver.java
+++ 2.1_module/src/task_three/coffeeMakerDriver.java
@@ -2,10 +2,22 @@ package task_three;
 
 public class coffeeMakerDriver {
     public static void main(String[] args) {
-        coffeeMaker myCoffee;
 
-        myCoffee = new coffeeMaker("espresso");
-        myCoffee.
+        coffeeMaker coffee = new coffeeMaker();
+        
+        coffee.switchOn();
+        System.out.println("coffee maker is on");
+
+
+        coffee.setType("espresso");
+        coffee.setAmount(20);
+
+        coffee.switchOff();
+        System.out.println("coffee maker is off");
+    
+
+        System.out.println("coffee type is: " + coffee.getcoffeeType());
+        System.out.println("coffee amount is: " + coffee.getcoffeeAmount() );
     }
     
 }
